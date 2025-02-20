c192a1f1e8cb99fadd51d6f67129d8d673d20766
Oliver Draheim <odr@telenot.de>
2025-02-20 22:19:21 +0100

    Added first sample page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {DataService} from "./services/data.service";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'tobuy-frontend';
+
+export class AppComponent implements OnInit {
+  data: any[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getData().subscribe((response) => {
+      this.data = response;
+    });
+  }
 }
